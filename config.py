d4831fdd910262e676650dc882201f1645c7fe66
2452355068 <153151061+2452355068@users.noreply.github.com>
2023-12-07 14:46:59 +0800

    init

---
+++ config.py
@@ -0,0 +1,11 @@
+# coding:utf-8 
+
+appid = '1256126892'
+secret_id = 'AKIDbQ4VhdMr89wDedFrIcgU2PaaMvOuBCzY'
+secret_key = 'PaXGl0ziY9UcWFjUyiFlCPMr77rLkJYlyA'
+bucket = 'face-recognize'
+
+APP_ID = '10365287'
+API_KEY = 'G7q4m36Yic1vpFCl5t46yH5K'
+SECRET_KEY = 'MneS2GDvPQ5QsGpVtSaHXGAlvwHu1XnC '
+domain='test.tbas.com'
