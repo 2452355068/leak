ed2e455eebc4c3676cb670cb475948999d5bbd93
2452355068 <153151061+2452355068@users.noreply.github.com>
2024-05-09 10:36:59 +0800

    init

---
+++ mysql/config.py
@@ -0,0 +1,11 @@
+# coding:utf-8 
+
+appid = '1256126892'
+host = 'mysql.tbas.com'
+port = '3306'
+username='root'
+password='root_passwd'
+
+APP_ID = '10365287'
+API_KEY = 'G7q4m36Yic1vpFCl5t46yH5K'
+SECRET_KEY = 'MneS2GDvPQ5QsGpVtSaHXGAlvwHu1XnC' 
