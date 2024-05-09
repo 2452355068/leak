910c8efbf4434d611792f43550b20ab318d150f3
2452355068 <153151061+2452355068@users.noreply.github.com>
2024-05-09 10:38:15 +0800

    init

---
+++ conf/aws/config.py
@@ -0,0 +1,12 @@
+# coding:utf-8 
+
+appid = '1256126892'
+secret_id = 'AKIAIOSFODNN7EXAMPLE'
+secret_key = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+'
+bucket = 'face-recognize'
+
+APP_ID = '10365287'
+API_KEY = 'G7q4m36Yic1vpFCl5t46yH5K'
+SECRET_KEY = 'MneS2GDvPQ5QsGpVtSaHXGAlvwHu1XnC '
+domain='test.tbas.com'
